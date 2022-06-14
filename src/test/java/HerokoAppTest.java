5403fe45c498a1d0e0ebae9d08e01a8bc2383d6d
sride <sridevi608@gmail.com>
2022-06-14 02:39:24 -0400

    added dynamic waits

--- src/test/java/HerokoAppTest.java
+++ src/test/java/HerokoAppTest.java
@@ -1,9 +1,13 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class HerokoAppTest {
     WebDriver driver;
@@ -19,13 +23,23 @@ public class HerokoAppTest {
     @Test
     public void test() {
         for (int i = 0; i < 10; i++) {
+            waitForElementToBeClickable(driver, "//button[text()='Add Element']");
             driver.findElement(By.xpath("//button[text()='Add Element']")).click();
-        }
-        for (int i = 1; i <= 10; i++) {
-            Assert.assertTrue(driver.findElement(By.xpath("(//button[text()='Delete'])[" + i + "]")).isDisplayed());
+            waitForPresenceOfElementLocated(driver, "(//button[text()='Delete'])[" + (i + 1) + "]");
+            Assert.assertTrue(driver.findElement(By.xpath("(//button[text()='Delete'])[" + (i + 1) + "]")).isDisplayed());
         }
         driver.close();
 
 
+    }
+
+    public void waitForElementToBeClickable(WebDriver driver, String locator) {
+        WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath(locator)));
+    }
+
+    public void waitForPresenceOfElementLocated(WebDriver driver, String locator) {
+        WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(locator)));
     }
 }
